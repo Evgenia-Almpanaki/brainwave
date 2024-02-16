58ac4f06f90a8741a4a5122416712ce7131780c1
Evgenia Almpanaki <evgenia.almpanaki@gmail.com>
2024-02-16 14:47:05 +0000

    Add Menu

---
+++ brainwave/src/app/menu.tsx
@@ -0,0 +1,22 @@
+const Menu = () => {
+
+    return (
+        <div className="d-flex flex-row justify-content-between p-4">
+            <div>Brainwave.io</div>
+            <div className="d-flex flex-row">
+                <MenuItem name={"Demos"} />
+                <MenuItem name={"Pages"} />
+                <MenuItem name={"Support"} />
+                <MenuItem name={"Contact"} />
+                <MenuItem name={"Sign In"} />
+                <MenuItem name={"Sign Up"} />
+            </div>
+        </div>
+    );
+}
+
+const MenuItem = ({ name }: { name: string }) => {
+    return (<div className="py-2 px-4">{name}</div>)
+}
+
+export default Menu;
--- brainwave/src/app/page.tsx
+++ brainwave/src/app/page.tsx
@@ -1,8 +1,9 @@
+import Menu from "./menu";
 import styles from "./page.module.css";
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
   return (
-    <></>
+    <Menu/>
   );
 }
